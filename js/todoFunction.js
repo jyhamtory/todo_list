f66d7b598ce98217c0c8072e43d9bd8558b40f7c
jyhamtory <140800004+jyhamtory@users.noreply.github.com>
2023-10-24 21:41:19 +0900

    Update todoFunction.js

--- js/todoFunction.js
+++ js/todoFunction.js
@@ -33,13 +33,24 @@ window.onload = function () {
 
       // 추가 후 input에 쓴 내용 초기화
       todoInput.value = "";
-
-      // 버튼 이벤트 추가 
-      check.addEventListener("click", checkTodo); // check 클릭하면 동작할 이벤트
-      text.addEventListener("dblclick", checkTodoClick); // text 더블클릭하면 동작할 이벤트 
-      del.addEventListener("click", delTodo); // del 클릭하면 동작할 이벤트
     }
   }
+  
+  // add버튼 클릭하면 동작할 이벤트
+  addBtn.addEventListener("click", addTodo)
+
+  //input 창에서 엔터 누르면 동작할 이벤트
+  todoInput.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();  //form 태그에 input 태그가 하나면 자동으로 submit. 이를 방지 하기 위해 추가
+      addTodo();
+    }
+  });
+  
+  // 버튼 이벤트 추가 
+  check.addEventListener("click", checkTodo); // check 클릭하면 동작할 이벤트
+  text.addEventListener("dblclick", checkTodoClick); // text 더블클릭하면 동작할 이벤트 
+  del.addEventListener("click", delTodo); // del 클릭하면 동작할 이벤트
 
 
   // check 클릭하면 동작할 이벤트
@@ -79,15 +90,4 @@ window.onload = function () {
     let list = e.target.parentElement.parentElement; //del의 부모요소(div)의 부모요소(li)
     list.remove();
   }
-
-  // add버튼 클릭하면 동작할 이벤트
-  addBtn.addEventListener("click", addTodo)
-
-  //input 창에서 엔터 누르면 동작할 이벤트
-  todoInput.addEventListener("keypress", function (e) {
-    if (e.keyCode === 13) {
-      e.preventDefault();  //form 태그에 input 태그가 하나면 자동으로 submit. 이를 방지 하기 위해 추가
-      addTodo();
-    }
-  });
 }
